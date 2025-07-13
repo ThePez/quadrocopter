fca68f821d82f5cd9c23e64ee24b3c6735ea6fc0
Jack <s4358260@student.uq.edu.au>
2025-07-13 20:02:16 +1000

    First go at driver for the hmc5883l compass

--- components/hmc5883l/hmc5883l.c
+++ components/hmc5883l/hmc5883l.c
@@ -16,7 +16,7 @@ void i2c_add_device(i2c_master_bus_handle_t bus) {
 
     i2c_device_config_t dev_cfg = {
         .dev_addr_length = I2C_ADDR_BIT_LEN_7, // Address bit length
-        .device_address = 0x58,                // Slave address
+        .device_address = 0x1E,                // Slave address
         .scl_speed_hz = 100000,                // 100kHz
     };
 
@@ -25,10 +25,38 @@ void i2c_add_device(i2c_master_bus_handle_t bus) {
 
 void magnetometer_init(i2c_master_bus_handle_t bus) {
     i2c_add_device(bus);
+
+    uint8_t config_a[2] = {HMC5883L_CONFIG_A_REG_ADDR, 0x70}; // 1 Sample Average, 15Hz, No Bias
+    uint8_t config_b[2] = {HMC5883L_CONFIG_B_REG_ADDR, 0x20}; // +/- 1.3 Ga (Default) 
+    uint8_t mode[2] = {HMC5883L_MODE_REG_ADDR, 0x00}; // Continuous measurement mode
+
+    // Write Register A
+    ESP_ERROR_CHECK(i2c_master_transmit(magnoI2CHandle, config_a, 2, -1));
+    // Write Register B
+    ESP_ERROR_CHECK(i2c_master_transmit(magnoI2CHandle, config_b, 2, -1));
+    // Write Mode Register
+    ESP_ERROR_CHECK(i2c_master_transmit(magnoI2CHandle, mode, 2, -1));
 }
 
-void magnetometer_read_register(uint8_t address) {
+uint8_t magnetometer_read_register(uint8_t address) {
+    uint8_t data = 0;
+    ESP_ERROR_CHECK(i2c_master_transmit_receive(magnoI2CHandle, &address, 1, &data, 1, 5));
+    return data;
 }
 
 void magnetometer_write_register(uint8_t address, uint8_t data) {
+    uint8_t toSend[2] = {address, data};
+    ESP_ERROR_CHECK(i2c_master_transmit(magnoI2CHandle, toSend, 2, 5));
 }
+
+void magnetometer_read_axis(int16_t* x, int16_t* y, int16_t* z) {
+    uint8_t addr = HMC5883L_OUTPUT_X_MSB_REG_ADDR;
+    uint8_t outputBuffer[6];
+
+    ESP_ERROR_CHECK(i2c_master_transmit_receive(magnoI2CHandle, &addr, 1, outputBuffer, 6, 5));
+    *x = ((int16_t) outputBuffer[0] << 8) | outputBuffer[1];
+    *z = ((int16_t) outputBuffer[2] << 8) | outputBuffer[3];
+    *y = ((int16_t) outputBuffer[4] << 8) | outputBuffer[5];
+}
+
+
--- components/hmc5883l/include/hmc5883l.h
+++ components/hmc5883l/include/hmc5883l.h
@@ -18,7 +18,24 @@
 #define MAGNO_SLAVE_ADDRESS_WRITE 0x3C
 #define MAGNO_SLAVE_ADDRESS_READ 0x3D
 
+#define HMC5883L_CONFIG_A_REG_ADDR 0x00
+#define HMC5883L_CONFIG_B_REG_ADDR 0x01
+#define HMC5883L_MODE_REG_ADDR 0x02
+#define HMC5883L_OUTPUT_X_MSB_REG_ADDR 0x03
+#define HMC5883L_OUTPUT_X_LSB_REG_ADDR 0x04
+#define HMC5883L_OUTPUT_Z_MSB_REG_ADDR 0x05
+#define HMC5883L_OUTPUT_Z_LSB_REG_ADDR 0x06
+#define HMC5883L_OUTPUT_Y_MSB_REG_ADDR 0x07
+#define HMC5883L_OUTPUT_Y_LSB_REG_ADDR 0x08
+#define HMC5883L_STATUS_REG_ADDR 0x09
+#define HMC5883L_ID_A_REG_ADDR 0x0A
+#define HMC5883L_ID_B_REG_ADDR 0x0B
+#define HMC5883L_ID_C_REG_ADDR 0x0C
+
 void i2c_add_device(i2c_master_bus_handle_t bus);
 void magnetometer_init(i2c_master_bus_handle_t bus);
+uint8_t magnetometer_read_register(uint8_t address);
+void magnetometer_write_register(uint8_t address, uint8_t data);
+void magnetometer_read_axis(int16_t* x, int16_t* y, int16_t* z);
 
 #endif
