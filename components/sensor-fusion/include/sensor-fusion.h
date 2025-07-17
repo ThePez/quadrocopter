4748ecf3a5535daa45214116b054fe40bd3f287c
Jack <s4358260@student.uq.edu.au>
2025-07-17 19:58:39 +1000

    Added sh2 Library for interfacing with the BNO085

---
+++ components/sensor-fusion/include/sensor-fusion.h
@@ -0,0 +1,17 @@
+/*
+ *****************************************************************************
+ * File: sensor-fusion.h
+ * Author: Jack Cairns
+ * Date: 17-07-2025
+ * Brief:
+ * REFERENCE: None
+ *****************************************************************************
+ */
+
+#ifndef SENSOR_FUSION_H
+#define SENSOR_FUSION_H
+
+#include "sh2.c"
+#include "sh2_util.h"
+
+#endif
